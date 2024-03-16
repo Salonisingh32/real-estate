9764e2415483c4405de3a08f012f9d872f72aeca
salonisingh32 <salonisingh6955@gmail.com>
2024-03-16 12:16:59 +0530

    install react.js and tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+// import{ BrowseRouter, Routes, Route} from 'react-router-dom';
+
+// export default function App() {
+//   return{
+
+//    <BrowseRouter>
+//    <Header></Header>
+//   <Routes>
+//     <Route path="/" element={<Home />}/>
+//     <Route path="/sign-in " element={<SignIn />}/>
+//     <Route path="/sign-up" element={<SignUp}/>
+//     <Route path="/about" element={<About />}/>
+//     <Route path="/profile" element={<Profile/>}/>
+//   </Routes>
+
+  
+
+
+
+//   </BrowseRouter>;
+// }
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>App</div>
+  )
+}
